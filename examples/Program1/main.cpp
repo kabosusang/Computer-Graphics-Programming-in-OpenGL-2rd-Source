f2dd6bf39a41f87caf7c7f400dba68921b4629ec
kabosusang <375487345@qq.com>
2025-05-14 19:54:05 +0800

    Add Programe01

---
+++ examples/Program1/main.cpp
@@ -0,0 +1,34 @@
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+using namespace std;
+
+void init(GLFWwindow *window) {}
+
+void display(GLFWwindow *window, double currentTime) {
+	glClearColor(1.0, 0.0, 0.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+}
+
+int main(void) {
+	if (!glfwInit()) {
+		exit(EXIT_FAILURE);
+	}
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	GLFWwindow *window = glfwCreateWindow(600, 600, "Chapter2 - program1", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+
+	init(window);
+
+	while (!glfwWindowShouldClose(window)) {
+		display(window, glfwGetTime());
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	exit(EXIT_SUCCESS);
+}
--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "glm/fwd.hpp"
-#include <glm/glm.hpp>
-#include <GLFW/glfw3.h>
-#include <SOIL2.h>
-#include <iostream>
-
-int main() {
-    std::cout << "Hello World!";
-}
